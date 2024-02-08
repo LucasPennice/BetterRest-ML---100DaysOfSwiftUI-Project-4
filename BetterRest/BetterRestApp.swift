f61b7e7793e261acb102d5d82b2fe93f82216e5d
Lucas Pennice <lucaspennice@gmail.com>
2024-02-08 19:51:12 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lucas Pennice on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
